aedc3c8b9b818761ce2eb2e4fb5509cbba300368
Elango-Hedonova <99873129+Elango-Hedonova@users.noreply.github.com>
2023-01-10 21:16:13 +0530

    Added starter files

---
+++ main.js
@@ -0,0 +1,28 @@
+const countryDetails = [
+  {
+    name: "India",
+    count: 50,
+    states: [
+      {
+        name: "Tamilnadu",
+        count: 10,
+      },
+      {
+        name: "Karnataka",
+        count: 10,
+      },
+      {
+        name: "Kerala",
+        count: 10,
+      },
+      {
+        name: "Maharastra",
+        count: 10,
+      },
+      {
+        name: "Punjab",
+        count: 10,
+      },
+    ],
+  },
+];
